c755fa69b05d8fafcc40696e1afd97ef870eb7d7
Pabl0Parra <pabsblue16@gmail.com>
2022-10-31 17:54:11 +0100

    app with no styles

--- src/App.js
+++ src/App.js
@@ -1,80 +1,98 @@
-import './app.css'
+import "./app.css";
 import { useEffect, useState } from "react";
 import {
-  RecordVoiceOverOutlined, 
+  RecordVoiceOverOutlined,
   VoiceOverOff,
-  SettingsOutlined
-} from '@mui/icons-material';
+  SettingsOutlined,
+} from "@mui/icons-material";
 import { useSpeechSynthesis } from "react-speech-kit";
-import Dialog from './Dialog'
+import Dialog from "./Dialog";
 
 const App = () => {
-  const [showSpeechSettings, setShowSpeechSettings] = useState(false)
-  const [highlightedText, setHighlightedText] = useState('')
+  const [showSpeechSettings, setShowSpeechSettings] = useState(false);
+  const [highlightedText, setHighlightedText] = useState("");
   const [voiceIndex, setVoiceIndex] = useState(null);
   const [pitch, setPitch] = useState(1);
   const [rate, setRate] = useState(1);
 
   const onEnd = () => {
-    setHighlightedText('')
-  }
-  const { speak, cancel, speaking, supported, voices } = useSpeechSynthesis({onEnd})
+    setHighlightedText("");
+  };
+  const { speak, cancel, speaking, supported, voices } = useSpeechSynthesis({
+    onEnd,
+  });
 
-  const voice = voices[voiceIndex] || null
+  const voice = voices[voiceIndex] || null;
 
   const handleTextSet = () => {
-    const text = window.getSelection().toString()
-    if(text !== '') setHighlightedText(text)
-  }
+    const text = window.getSelection().toString();
+    if (text !== "") setHighlightedText(text);
+  };
 
   // setting highlightedText state when text has been highlighted
   useEffect(() => {
-    document.addEventListener('mouseup', handleTextSet)
+    document.addEventListener("mouseup", handleTextSet);
     return () => {
-      document.removeEventListener('mouseup', handleTextSet)
-    }
-  }, [])
+      document.removeEventListener("mouseup", handleTextSet);
+    };
+  }, []);
 
   return (
     <div className="app">
       <h1>Text to Speech</h1>
-      <span>Highligh text and click the speack icon to listen to highlighted text</span>
+      <span>
+        Highligh text and click the speack icon to listen to highlighted text
+      </span>
+
       <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing 
-        software like Aldus PageMaker including versions of Lorem Ipsum.
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum ha sido el texto ficticio estándar de la industria
+        desde el siglo XVI, quando uno stampatore sconosciuto ha preso una
+        cambusa di caratteri e l'ha rimescolata per creare un libro campione di
+        caratteri. それは 5
+        世紀だけでなく、電子組版への飛躍にも生き残り、本質的に変わっていません。
+        Sie hat nicht nur fünf Jahrhunderte, sondern auch den Sprung in den
+        elektronischen Satz überstanden und ist im Wesentlichen unverändert
+        geblieben.
       </p>
 
-      {supported && 
+      {supported && (
         <div className="speechMenu">
-          {!speaking
-            ? <RecordVoiceOverOutlined onClick={() => speak({ text: highlightedText, voice, rate, pitch})}/>
-            : <VoiceOverOff onClick={cancel}/>
-          }
-          <SettingsOutlined onClick={() => setShowSpeechSettings(true)}/>
+          {!speaking ? (
+            <RecordVoiceOverOutlined
+              onClick={() =>
+                speak({ text: highlightedText, voice, rate, pitch })
+              }
+            />
+          ) : (
+            <VoiceOverOff onClick={cancel} />
+          )}
+          <SettingsOutlined onClick={() => setShowSpeechSettings(true)} />
         </div>
-      }
+      )}
 
-      <Dialog open={showSpeechSettings} onClose={() => setShowSpeechSettings(false)}>
-        <div className='speechSettings'>
+      <Dialog
+        open={showSpeechSettings}
+        onClose={() => setShowSpeechSettings(false)}
+      >
+        <div className="speechSettings">
           {/* VOices -- browser dependent */}
           <select
             name="voice"
-            value={voiceIndex || ''}
+            value={voiceIndex || ""}
             onChange={(e) => {
-              setVoiceIndex(e.target.value)
+              setVoiceIndex(e.target.value);
             }}
           >
             {voices.map((option, index) => (
               <option key={option.voiceURI} value={index}>
-                {`${option.lang} - ${option.name} ${ option.default ? '- Default' : ''}`}
+                {`${option.lang} - ${option.name} ${
+                  option.default ? "- Default" : ""
+                }`}
               </option>
             ))}
           </select>
-          <div className='rangeContainer'>
+          <div className="rangeContainer">
             <div>
               <label htmlFor="rate">Rate: </label>
               <span>{rate}</span>
@@ -90,7 +108,7 @@ const App = () => {
               }}
             />
           </div>
-          <div className='rangeContainer'>
+          <div className="rangeContainer">
             <div>
               <label htmlFor="pitch">Pitch: </label>
               <span>{pitch}</span>
@@ -109,8 +127,8 @@ const App = () => {
           </div>
         </div>
       </Dialog>
-    </div>    
-  )
-}
+    </div>
+  );
+};
 
 export default App;
